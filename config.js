fd4e51ee5d68ac257ae587da0f0f748f6de6ebd2
peskyphenix <madscuup@gmail.com>
2026-02-02 16:30:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['💩', '👀'],  // Heart emojis
+        hearts: ['💩', '👀','🌻', '👀''🪻''🌼'],  // Heart emojis
         bears: ['👀', '🫶'], // Cute bear emojis
     },
 
